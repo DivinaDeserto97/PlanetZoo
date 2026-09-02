ccae7b50628973f1daa729df23a9737c93e48503
DivinaDeserto97 <divina.deserto20@gmail.com>
2026-09-02 12:45:49 +0200

    neustart

--- assets/js/normalisierung.js
+++
@@ -1,9 +0,0 @@
-export function normalisiereText(wert) {
-  return String(wert || "")
-    .trim()
-    .toLowerCase()
-    .replaceAll("ä", "ae")
-    .replaceAll("ö", "oe")
-    .replaceAll("ü", "ue")
-    .replaceAll("ß", "ss");
-}
